500ca3b5dee4a78020d181f6e8ea38714469b897
eminaydinalp <muhammetemin4444@gmail.com>
2021-11-09 12:33:51 +0300

    commit

--- Assets/EndlessGame/Scripts/Powerup/PowerupController.cs
+++ Assets/EndlessGame/Scripts/Powerup/PowerupController.cs
@@ -6,14 +6,10 @@ using UnityEngine.UI;
 public class PowerupController : MonoBehaviour
 {
 	public Button[] buttons;
-	//public Dictionary<IPowerup, float> activePowerups = new Dictionary<IPowerup, float>();
+	public Dictionary<string, float> activePowerups = new Dictionary<string, float>();
 
-	public List<IPowerup> activePowerUps = new List<IPowerup>();
-	//public List<string> names = new List<string>();
-
-
-	// Handles the beginning and ending of activated Powerups.
-	// Inactive Powerups are removed automatically.
+	public List<IPowerup> powerupList = new List<IPowerup>();
+	
 	void Update()
 	{
 		HandleGlobalPowerups();
@@ -21,57 +17,35 @@ public class PowerupController : MonoBehaviour
 	private void HandleGlobalPowerups()
 	{
 
-		foreach (IPowerup powerup in activePowerUps)
+		foreach (IPowerup powerup in powerupList)
 		{
-			if (powerup.Duration > 0)
+			if (activePowerups[powerup.Name] > 0)
 			{
 				buttons[powerup.Index].interactable = true;
-				buttons[powerup.Index].gameObject.transform.GetChild(0).GetComponent<Text>().text = "" + ((int)powerup.Duration);
-				powerup.Duration -= Time.deltaTime;
+				buttons[powerup.Index].gameObject.transform.GetChild(0).GetComponent<Text>().text = "" + ((int)activePowerups[powerup.Name]);
+				activePowerups[powerup.Name] -= Time.deltaTime;
 			}
 			else
 			{
 				buttons[powerup.Index].interactable = false;
-				activePowerUps.Remove(powerup);
-				//names.Remove(powerup.Name);
-				//count.Remove(activePowerups[powerup]);
-				//if (powerup.endAction != null)
+				powerupList.Remove(powerup);
+				activePowerups.Remove(powerup.Name);
 				powerup.EndProcess();
 			}
 		}
 	}
 
-	// Adds a global Powerup to the activePowerups list.
 	public void ActivatePowerup(IPowerup powerup)
 	{
-		//if (!activePowerups.ContainsKey(powerup))
-		//{
-		//    Debug.Log(powerup);
-		//    powerup.StartProcess();
-		//    activePowerups.Add(powerup, powerup.Duration);
-		//}
-		//else
-		//{
-		//    activePowerups[powerup] += powerup.Duration;
-		//}
-
-		//keys = new List<IPowerup>(activePowerups.Keys);
-
-		if (activePowerUps == null)
+		if (!activePowerups.ContainsKey(powerup.Name))
 		{
 			powerup.StartProcess();
-			activePowerUps.Add(powerup);
-			//names.Add(powerup.Name);
+			activePowerups.Add(powerup.Name, powerup.Duration);
+			powerupList.Add(powerup);
 		}
 		else
 		{
-			for (int i = 0; i < activePowerUps.Count; i++)
-			{
-				if (activePowerUps[i].Name == powerup.Name)
-				{
-					activePowerUps[i].Duration += powerup.Duration;
-				}
-			}
+			activePowerups[powerup.Name] += powerup.Duration;
 		}
 
 	}
